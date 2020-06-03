9486c3dd7f8d40205d88a4cf2c4dd58b3ac80b70
inada <starmelt190@gmail.com>
2020-06-03 14:33:45 +0900

    環境変数の編集実施3

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -78,7 +78,7 @@ RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :selenium, using: :headless_chrome, options: {
       browser: :remote,
-      url: ENV.fetch("SELENIUM_DRIVER_URL", 'http://selenium_chrome:4444/wd/hub'),
+      url: ENV.fetch("SELENIUM_DRIVER_URL", 'localhost'),
       desired_capabilities: :chrome
     }
     Capybara.server_host = 'web'
