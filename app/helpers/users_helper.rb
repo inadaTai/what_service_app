e4ec7c8200e5c0883126d5a3e00fb2a3943a61de
inada <starmelt190@gmail.com>
2020-06-14 15:55:44 +0900

    CSSデザイン修正を実施

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,2 @@
 module UsersHelper
-  def gravatar_for(user, size: 80)
-    if user.email.nil?
-      image_tag("/default.jpg", :size => 50, alt: user.name, class: "gravatar")
-    else
-      gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-      image_tag(gravatar_url, alt: user.name, class: "gravatar")
-    end
-  end
 end
