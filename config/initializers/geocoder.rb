8cd8643a89417ee0fcbed4621bfa9ba5837251e7
inada <starmelt190@gmail.com>
2020-06-19 16:04:52 +0900

    テザイン修正、geocorderの精度を上げました。

---
+++ config/initializers/geocoder.rb
@@ -0,0 +1,4 @@
+Geocoder.configure(
+  lookup: :google,
+  api_key: 'AIzaSyCsLCUY_Ms5omJIG7V2-25ioMk49a-symM'
+)
--- spec/systems/comments_spec.rb
+++ spec/systems/comments_spec.rb
@@ -2,34 +2,21 @@ require 'rails_helper'
 
 RSpec.describe "Comments", type: :system do
   let(:user) { create(:user) }
-  let(:micropost) { create(:micropost) }
 
   def submit_valid_micropost
     visit post_pages_path
-    fill_in '記事の題名', with: 'フィットネスクラブ', match: :first
-    fill_in 'サービスの金額を入力してください', with: '月額500円', match: :first
+    fill_in '(※必須)記事の題名', with: '渋谷のフィットネスクラブ', match: :first
+    fill_in '(※必須)サービスの金額を入力してください', with: '月額500円', match: :first
     find('trix-editor').click.set('良いサービス')
     attach_file 'micropost[picture]', "#{Rails.root}/db/images_seeds/1.png", match: :first
     click_button '投稿する'
   end
 
   def submit_valid_comment
-    click_on 'フィットネスクラブ'
+    click_on '渋谷のフィットネスクラブ'
     fill_in "comment_body", with: 'a' * 140
     click_on 'コメント投稿'
     visit current_path
-  end
-
-  context "未ログイン時の表示" do
-    it "未ログインのユーザーはコメント入力欄がない" do
-      login_system(user)
-      submit_valid_micropost
-      link = find('#picture_dropdown')
-      link.click
-      click_on 'ログアウト'
-      click_on 'フィットネスクラブ'
-      expect(page).not_to have_content "comment_body"
-    end
   end
 
   context "ログイン済み時の表示" do
@@ -39,26 +26,39 @@ RSpec.describe "Comments", type: :system do
     end
 
     it "空欄で無効なコメントを行いエラーがメッセージが出る" do
-      click_on 'フィットネスクラブ'
+      click_on '渋谷のフィットネスクラブ'
       click_on 'コメント投稿'
       expect(page).to have_selector '.alert-danger'
     end
 
     it "141文字以上の無効なコメントを行いエラーメッセージが出る" do
-      click_on 'フィットネスクラブ'
+      click_on '渋谷のフィットネスクラブ'
       fill_in "comment_body", with: 'a' * 141
       click_on 'コメント投稿'
       expect(page).to have_selector '.alert-danger'
     end
 
-    it "コメント投稿者は自分の投稿を削除できコメントのカウントも減る" do
+    it "コメント投稿者は自分の投稿を削除できコメントのカウントも減る", js: true do
       submit_valid_comment
       click_on 'コメント削除'
       expect { visit current_path }.to change(user.comments, :count).by(-1)
     end
 
-    it "有効なコメント投稿を行うとコメントのカウントも増える" do
+    it "有効なコメント投稿を行うとコメントのカウントも増える", js: true do
       expect { submit_valid_comment }.to change(user.comments, :count).by(1)
+    end
+  end
+  
+  context "未ログイン時の表示" do
+
+    it "未ログインのユーザーはコメント入力欄がない" do
+      login_system(user)
+      submit_valid_micropost
+      link = find('#picture_dropdown')
+      link.click
+      click_on 'ログアウト'
+      click_on '渋谷のフィットネスクラブ'
+      expect(page).not_to have_content "comment_body"
     end
   end
 end
